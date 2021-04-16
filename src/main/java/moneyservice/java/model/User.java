af7bcdf8be88b3ffff2e4e405d1b41623184ea8c
linnfaa <linnfaa.jacobsen@gmail.com>
2021-04-16 11:59:27 +0200

    Added new class User

---
+++ src/main/java/moneyservice/java/model/User.java
@@ -0,0 +1,32 @@
+package moneyservice.java.model;
+
+public class User {
+	
+	// ATTRIBUTES
+	
+	private String name;	// final?
+	
+	
+	// DEFAULT CONSTRUCTOR
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	
+	// METHODS
+	
+	public Order createOrderRequest() {
+		
+	}
+
+	// GETTERS AND SETTERS
+
+	public String getName() {
+		return name;
+	}
+	
+	
+	// TOSTRING ??
+	
+}
