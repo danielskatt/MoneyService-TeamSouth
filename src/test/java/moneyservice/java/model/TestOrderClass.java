885b7df3772c0700323f00660317a8620d5e5603
Zaur Aslakhanov <z.asla@hotmail.com>
2021-04-16 22:14:29 +0200

    Added test cases for Order class

--- src/test/java/moneyservice/java/model/TestOrderClass.java
+++ src/test/java/moneyservice/java/model/TestOrderClass.java
@@ -2,13 +2,55 @@ package moneyservice.java.model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestOrderClass {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOrderConstructor1() {
+		Order od = new Order("South","USD",100, TransactionMode.BUY);
+		assertNotNull(od);
+	}
+	
+	@Test
+	public void testOrderConstructor2() {
+		Order od = new Order("South","APP",100, TransactionMode.BUY);
+		assertNotNull(od);
+	}
+	
+	@Test
+	public void testOrderConstructor3() {
+		Order od = new Order("South","WON",100, TransactionMode.SELL);
+		assertNull(od);
+	}
+	
+	@Test
+	public void testGetSite1() {
+		Order od = new Order("South","USD",100, TransactionMode.BUY);
+		String siteName = od.getSite();
+		assertTrue(siteName.equals("South"));
+	}
+	
+	@Test
+	public void testGetCurrencyCode1() {
+		Order od = new Order("South","USD",100, TransactionMode.BUY);
+		String currencyCode = od.getCurrencyCode();
+		assertTrue(currencyCode.equals("USD"));
+	}
+	
+	@Test
+	public void testGetAmount1() {
+		Order od = new Order("South","USD",100, TransactionMode.BUY);
+		int amount = od.getAmount();
+		assertEquals(amount, 100);
+	}
+	
+	@Test
+	public void testGetMode() {
+		Order od = new Order("South","USD",100, TransactionMode.BUY);
+		TransactionMode aMode = od.getMode();
+		assertTrue(aMode, TransactionMode.BUY);
 	}
 
 }
