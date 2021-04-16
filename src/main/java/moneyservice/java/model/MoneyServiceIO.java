1b0588d22c2fd0086a5c3707a327540e94d18439
linnfaa <linnfaa.jacobsen@gmail.com>
2021-04-16 11:48:14 +0200

    Added new class MoneyServiceIO

---
+++ src/main/java/moneyservice/java/model/MoneyServiceIO.java
@@ -0,0 +1,19 @@
+package moneyservice.java.model;
+
+import java.util.List;
+
+public class MoneyServiceIO {
+	
+	// METHODS
+	
+	// public or package, return boolean?
+	public static void storeTransactionsAsSer(List<Transaction> transactionList) {
+//	static boolean storeTransactionsAsSer(String filename, List<Transaction> transactionList) {
+		
+	}
+	
+	// public or package?
+	static List<Transaction> readReportAsSer(String filename) {
+		
+	}
+}
