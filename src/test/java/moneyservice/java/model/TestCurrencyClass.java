3443a204caa484094fd30c3de9231c6df64dffce
Zaur Aslakhanov <z.asla@hotmail.com>
2021-04-18 22:26:23 +0200

    added new test cases for currency class

--- src/test/java/moneyservice/java/model/TestCurrencyClass.java
+++ src/test/java/moneyservice/java/model/TestCurrencyClass.java
@@ -7,8 +7,28 @@ import org.junit.Test;
 public class TestCurrencyClass {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCurrencyConstructor1() {
+		String code = "USD";
+		float rate = 9.23F;
+		Currency testCurrency = new Currency(code,rate);
+		
+		assertNotNull(testCurrency);
+	}
+	
+	@Test
+	public void testGetCurrencyCode1() {
+		Currency testCurrency = new Currency("USD",9.22F);
+		String theCurrencyCode = testCurrency.getCurrencyCode();
+		
+		assertTrue(theCurrencyCode.equals("USD"));
+	}
+	
+	@Test
+	public void testGetRate1() {
+		Currency testCurrency = new Currency("USD",9.22F);
+		float theRate = testCurrency.getRate();
+		
+		assertEquals(theRate,9,22F);
 	}
 
 }
