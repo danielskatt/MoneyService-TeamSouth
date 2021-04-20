46d82e32e199b1a0c6bfb2117941656872068731
danielskatt <daniel.skatt@gmail.com>
2021-04-20 07:58:45 +0200

    fixed some bugs in test code

--- src/test/java/moneyservice/java/model/TestTransactionClass.java
+++ src/test/java/moneyservice/java/model/TestTransactionClass.java
@@ -10,35 +10,35 @@ public class TestTransactionClass {
 
 	@Test
 	public void testCreateTransaction1() {
-		Order od = new Order("South","USD",100,TransactionMode.BUY)
-		Transaction aTransaction = new Transaction(od)
+		Order od = new Order("South","USD",100,TransactionMode.BUY);
+		Transaction aTransaction = new Transaction(od);
 		assertNotNull(aTransaction);
 	}
 	
 	
 	@Test
 	public void testGetId1() {
-		Order od = new Order("South","USD",100,TransactionMode.BUY)
-		Transaction aTransaction = new Transaction(od)
+		Order od = new Order("South","USD",100,TransactionMode.BUY);
+		Transaction aTransaction = new Transaction(od);
 		int id = aTransaction.getId();
 		assertEquals(id,2);
 	}
 	
 	@Test
 	public void testGetTimeStamp1() {
-		Order od = new Order("South","USD",100,TransactionMode.BUY)
+		Order od = new Order("South","USD",100,TransactionMode.BUY);
 		LocalDateTime testDateTime = LocalDateTime.now();
 
-		Transaction aTransaction = new Transaction(od)
+		Transaction aTransaction = new Transaction(od);
 
-		assertTrue(testDateTime.isBefore(aTransaction.getTimeStamp().getDate()) ||
-				testDateTime.isEqual(aTransaction.getTimeStamp().getDate()));
+		assertTrue(testDateTime.isBefore(aTransaction.getTimeStamp()) ||
+				testDateTime.isEqual(aTransaction.getTimeStamp()));
 	}
 
 	@Test
 	public void testGetCurrencyCode1() {
-		Order od = new Order("South","USD",100,TransactionMode.BUY)
-		Transaction aTransaction = new Transaction(od)
+		Order od = new Order("South","USD",100,TransactionMode.BUY);
+		Transaction aTransaction = new Transaction(od);
 		String currencyCode = aTransaction.getCurrencyCode();
 		
 		assertTrue(currencyCode.equals("USD"));
@@ -47,7 +47,7 @@ public class TestTransactionClass {
 	@Test
 	public void testGetAmount1() {
 		Order od = new Order("South","USD",100,TransactionMode.BUY);
-		Transaction aTransaction = new Transaction(od)
+		Transaction aTransaction = new Transaction(od);
 		int amount = aTransaction.getAmount();
 		
 		assertEquals(amount, 100);
@@ -60,7 +60,7 @@ public class TestTransactionClass {
 		
 		TransactionMode mode = aTransaction.getMode();
 		
-		assertTrue(mode, TransactionMode.BUY);
+		assertEquals(mode, TransactionMode.BUY);
 	}
 
 }
