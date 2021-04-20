1b26b108a76e6165c26b6c7b53759e795de7fd05
Fredrik Blomgren <blomgren_fredrik@hotmail.com>
2021-04-20 15:45:29 +0200

    Did a draft for automatic order flow

--- Site/src/main/java/moneyservice/site/app/MoneyServiceApp.java
+++ Site/src/main/java/moneyservice/site/app/MoneyServiceApp.java
@@ -1,5 +1,8 @@
 package moneyservice.site.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import moneyservice.model.Configuration;
 import moneyservice.model.Order;
 import moneyservice.model.Site;
@@ -28,8 +31,35 @@ public class MoneyServiceApp {
 		Order order = createOrder(user);
 		boolean store = handleOrder(order);
 		
+		int numberOfOrders = 25;
+		int numberOfDays = 20;
 		
-	}
+		List<Order> orderList = new ArrayList<Order>();
+		
+		for(int i=0;i<numberOfDays;i++) {
+			for(int k=0;k<numberOfOrders;k++) {
+				Order order1 = createOrder(user);
+				if(!(order1.equals(null)))
+					orderList.add(order1);
+			}
+		}
+		
+		
+		for(Order temp : orderList) {
+			boolean orderApproved = handleOrder(temp);
+			
+			if(!orderApproved) {
+				// TODO: Replace print out with Logging file
+				System.out.println("Order not approved: "+temp.toString());
+			}
+			
+			if(orderApproved) {
+				// TODO: Replace print out with Logging file
+				System.out.println("Order not approved: "+temp.toString());
+			}
+		}
+		
+	}	
 	/**
 	 * Helper method for creating a User
 	 * @return - 
