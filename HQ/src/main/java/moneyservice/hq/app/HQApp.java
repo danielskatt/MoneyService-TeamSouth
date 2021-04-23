eb631da8822cfd83ea0fcb4f2638681cfdfd7da0
linnfaa <linnfaa.jacobsen@gmail.com>
2021-04-23 21:48:29 +0200

    First draft for getting all reports from folders

--- HQ/src/main/java/moneyservice/hq/app/HQApp.java
+++ HQ/src/main/java/moneyservice/hq/app/HQApp.java
@@ -1,9 +1,14 @@
 package moneyservice.hq.app;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Optional;
 import java.util.Scanner;
 
+import moneyservice.model.MoneyServiceSites;
+
 public class HQApp {
-	
+
 	// CONSTANTS
 	static Scanner keyboard = new Scanner(System.in);
 	private static final int EXIT = 0;
@@ -13,9 +18,42 @@ public class HQApp {
 	private static final int SITE_MENU_MAX = 5;
 
 	public static void main(String[] args) {
-		
+
+		// get directory path for HQ project
+		String HQdirPath = System.getProperty("user.dir");
+		System.out.println(HQdirPath);	// DEBUG
+
+		// iterate through Sites
+		for(MoneyServiceSites aSite: MoneyServiceSites.values() ) {
+
+			// get transaction directory path for each site
+			String siteDirPath = HQdirPath + File.separator + aSite;
+			System.out.println(siteDirPath);	// DEBUG
+
+			// creates a file object
+			File f = new File(siteDirPath);
+			String[] fileNames;
+
+
+			// This filter will only include files ending with .ser
+			FilenameFilter filter = new FilenameFilter() {
+				@Override
+				public boolean accept(File f, String name) {
+					return name.endsWith(".ser");
+				}
+			};
+
+			// get all filenames that end with .ser
+			fileNames = f.list(filter);
+			
+			if(fileNames != null) {		// BUG
+				for(String fn: fileNames) {
+					System.out.println(fn);		// DEBUG
+				}
+			}
+		}
 	}
-	
+
 	/**
 	 * Gets unsigned number from user input. If entry is not valid return value equals to -1
 	 * @return num an int >= 0 OR -1 if input is invalid
@@ -37,7 +75,7 @@ public class HQApp {
 		System.out.println(input + " is not a valid number!");
 		return errorNo;
 	}
-	
+
 	/**
 	 * This method gets user input for choice of Site
 	 * @return userSiteInput an int defining the choosen site:
@@ -73,7 +111,7 @@ public class HQApp {
 				System.out.println(userSiteInput + " is not a menu choice!");
 				System.out.format("%nEnter your choice (%d-%d): ", SITE_MENU_MIN, SITE_MENU_MAX);
 			}
-			
+
 			if(userSiteInput == -1) {
 				System.out.format("%nEnter your choice (%d-%d): ", SITE_MENU_MIN, SITE_MENU_MAX);
 			}
@@ -82,7 +120,7 @@ public class HQApp {
 
 		return userSiteInput;
 	}
-	
+
 	/**
 	 * This method gets user input for period 
 	 * @return userPeriodInput an int defining the choosen period:
@@ -101,7 +139,7 @@ public class HQApp {
 		System.out.format("2 - Week%n");
 		System.out.format("3 - Month%n");
 		System.out.format("0 - Exit%n");	// EXIT
-		
+
 		System.out.println();
 		System.out.format("Enter your choice: ");
 
@@ -115,7 +153,7 @@ public class HQApp {
 				System.out.println(userPeriodInput + " is not a menu choice!");
 				System.out.format("%nEnter your choice (%d-%d): ", PERIOD_MENU_MIN, PERIOD_MENU_MAX);
 			}
-			
+
 			if(userPeriodInput == -1) {		// invalid input
 				System.out.format("%nEnter your choice (%d-%d): ", PERIOD_MENU_MIN, PERIOD_MENU_MAX);
 			}
