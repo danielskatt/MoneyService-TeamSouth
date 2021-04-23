02db83a83e9f4194e24af13a1b55b51e63843d4f
linnfaa <linnfaa.jacobsen@gmail.com>
2021-04-23 11:30:37 +0200

    Added new enum class for collecting all Sites

---
+++ HQ/src/main/java/moneyservice/model/MoneyServiceSites.java
@@ -0,0 +1,46 @@
+package moneyservice.model;
+
+public enum MoneyServiceSites {
+	NONE("None", 0),
+	NORTH("North", 1),
+	EAST("East", 2),
+	CENTER("Center", 3),
+	SOUTH("South", 4),
+	ALL("All", 5);
+	
+	/**
+	 * @attribute name a String defining name of Site
+	 */
+	private final String name;
+	/**
+	 * @attribute numVal an int defining the numerical value
+	 */
+	private final int numVal;
+	
+	
+	/**
+	 * Default constructor
+	 * @param name
+	 * @param numVal
+	 */
+	MoneyServiceSites(String name, int numVal) {
+		this.name = name;
+		this.numVal = numVal;
+	}
+	
+	/**
+	 * Getter for String attribute name
+	 * @return name a String defining name of Site
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Getter for int attribute numVal
+	 * @return numVal an int defining the numerical value
+	 */
+	public int getNumVal() {
+		return numVal;
+	}
+}
