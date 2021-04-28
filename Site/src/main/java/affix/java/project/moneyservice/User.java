a1414c1ea38ce73ae8d146974efdf83157f76885
Fredrik Blomgren <blomgren_fredrik@hotmail.com>
2021-04-29 00:03:12 +0200

    Added user input to class user

--- Site/src/main/java/affix/java/project/moneyservice/User.java
+++ Site/src/main/java/affix/java/project/moneyservice/User.java
@@ -1,9 +1,11 @@
 package affix.java.project.moneyservice;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.Scanner;
 
 /**---------------User Class-------------------
  * Holds information about a specific user such as the name.
@@ -22,6 +24,71 @@ public class User {
 	 */
 	public User(String name) {
 		this.name = name;
+	}
+	
+	/**
+	 * method made for 
+	 * @return Optional - Of type Order
+	 */
+	public Optional<Order> userCreatedOrder(){
+		
+		Scanner userInput = new Scanner(System.in);
+		Order userOrder = null;
+		TransactionMode tmode = null;
+		int amount = 0;
+		String code = null;
+		String mode = null; 
+		String site = null;
+		boolean accepted = false;
+		int i = 0;
+		System.out.format("\n Enter following data: \n");
+		while(!accepted) {
+			try {
+				i=0;
+				System.out.format("\nEnter requested site: ");
+				String input = userInput.next();
+				if(input.equalsIgnoreCase("South")||input.equalsIgnoreCase("North")||input.equalsIgnoreCase("West")||input.equalsIgnoreCase("East"))
+				{	site =  input; i++; }
+				
+				System.out.format("\nEnter mode (Buy/Sell): ");
+				input = userInput.next();
+				if(input.equalsIgnoreCase("Buy")||input.equalsIgnoreCase("Sell"))
+				{	mode = input; i++; }
+					
+				System.out.format("\nEnter currency code (USD,AUD etc): ");
+				input = userInput.next();
+				if(input.matches("^[A-Z]*$"))
+				{	code = input; i++; }
+				
+				System.out.format("\nEnter amount: ");
+				amount = userInput.nextInt();
+				
+				if(!(mode==null)) {
+				if(mode.equalsIgnoreCase("Buy"))
+				tmode = TransactionMode.BUY;
+			
+				if(mode.equalsIgnoreCase("Sell"))
+				tmode = TransactionMode.SELL;
+				}
+			if(i==3) {
+				accepted = true;
+			}
+			else {
+				System.out.format("\nWrong inputs in either: Currency code, Buy or Sell, Site");
+			}
+				
+			}catch(InputMismatchException e) {
+				site = null;
+				mode = null;
+				code = null;
+				System.out.format("\n Wront input!");
+				userInput.nextLine();
+			}
+		}
+		
+		userOrder = new Order(site,code,amount,tmode);
+		
+		return Optional.of(userOrder);	
 	}
 	
 	/** 
@@ -46,7 +113,6 @@ public class User {
 
 		//Order creation based on randomized data.
 		Order test = new Order(Site,currencyTicker,amount,transMode);
-		System.out.println(test); // For display purposes only, will be removed later on.
 		Optional<Order> theOrder= Optional.of(test);
 		
 		return theOrder;
