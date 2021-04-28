1637e6c010e393ae861ad575578bf35cdb183aab
linnfaa <linnfaa.jacobsen@gmail.com>
2021-04-28 17:51:12 +0200

    Refactored and documented order class. Added tests for Order class

--- Site/src/main/java/affix/java/project/moneyservice/Order.java
+++ Site/src/main/java/affix/java/project/moneyservice/Order.java
@@ -2,159 +2,212 @@ package affix.java.project.moneyservice;
 
 import java.util.logging.Logger;
 
-/** -------------------- Order ----------------------
- * <p>
- *  Holds information about an Order. 
- *  Can only be created by creating an Order object 
- *  with all necessary information available.
- * <p>
- * --------------------------------------------------*/
+/** 
+ * This class defines a Order in MoneyService
+ * Order should be created by specifying site name, currency code, the amount and 
+ * transaction mode, buy or sell. 
+ * NB! TransactionMode is seen from customer perspective e.g. if a customer wants to 
+ * buy Euro, TransactionMode should be BUY.
+ */
+
 public class Order implements Comparable<Order> {
+	
 	/**
-	 * @attribute site - Holds information about which Site the order is intended for
+	 * @attribute logger a Logger
+	 */
+	private static Logger logger;
+	/**
+	 * Setter for attribute logger
+	 */
+	static {logger = Logger.getLogger("affix.java.project.moneyservice");}
+	
+	/**
+	 * @attribute site a String holding the name of the Money Service site Order is intended for
 	 */
 	private final String site;
+	
+	// capital and 3 letters
 	/**
-	 * @attribute currencyCode - Holds information about which currencyCode (example USD) for the Order 
+	 * @attribute currencyCode a String defining the code of the currency e.g. USD or EUR to order
 	 */
 	private final String currencyCode;
+	
 	/**
-	 * @attribute amount - Holds information about the amount of currencyCode for the Order 
+	 * @attribute amount an int holding the amount of currency to order
 	 */
 	private final int amount;
+	
 	/**
-	 * @attribute transactionMode - Holds information about if the type of Order, for example Sell or Buy
+	 * @attribute transactionMode a TransactionMode (Enum) defining if customer want to sell or buy currency
 	 */
 	private final TransactionMode transactionMode;
 	
-	private static Logger logger;
-	
-	static{
-		logger = Logger.getLogger("affix.java.project.moneyservice");
-	}
 	
 	/**
-	 * Overloaded constructor
-	 * @param site - Defines the specific Site the Order is intended for
-	 * @param currencyCode - Defines the code for the currency
-	 * @param amount - Defines the amount of the currencyCode for the order
-	 * @param transactionMode - Defines the type of Order
+	 * Default constructor for creating a complete Order object
+	 * @param site a String holding the name of the Money Service site Order is intended for
+	 * @param currencyCode a String defining the code of the currency e.g. USD or EUR to order
+	 * @param amount an int holding the amount of currency to order
+	 * @param transactionMode a TransactionMode (Enum) defining if customer want to sell or buy currency
+	 * @throws IllegalArgumentException if parameters don't match requirements 
 	 */
 	public Order(String site, String currencyCode, int amount, TransactionMode transactionMode) {
-		if(amount <= 0) {
-			throw new IllegalArgumentException("Amount cannot be 0 or lower!");
+		if(site.isEmpty()) {
+			throw new IllegalArgumentException("Site name can NOT be empty!");
 		}
+		
+		if(!(currencyCode.length() == 3 && currencyCode.matches("^[A-Z]*$"))) {
+			throw new IllegalArgumentException("Site name can NOT be empty!");
+		}
+		
+		if(amount <= 0) {
+			throw new IllegalArgumentException("Amount con NOT be 0 or less than 0!");
+		}
+		
 		this.site = site;
 		this.currencyCode = currencyCode;
 		this.amount = amount;
 		this.transactionMode = transactionMode;
 	}
 	
+	
 	/**
-	 * 
-	 * @return site
+	 * Getter for attribute site
+	 * @return site a String holding the name of the Money Service site Order is intended for
 	 */
 	public String getSite() {
 		return site;
 	}
 
 	/**
-	 * 
-	 * @return currencyCode
+	 * Getter for attribute currencyCode
+	 * @return currencyCode a String defining the code of the currency e.g. USD or EUR to order
 	 */
 	public String getCurrencyCode() {
 		return currencyCode;
 	}
 
 	/**
-	 * 
-	 * @return amount
+	 * Getter for attribute amount
+	 * @return amount an int holding the amount of currency to order
 	 */
 	public int getAmount() {
 		return amount;
 	}
 	
 	/**
-	 * 
-	 * @return transactionMode
+	 * Getter for attribute transactionMode
+	 * @return transactionMode a TransactionMode (Enum) defining if customer want to sell or buy currency
 	 */
 	public TransactionMode getTransactionMode() {
 		return transactionMode;
 	}
 
-	/**
-	 * toString - returns a String with information about an Order
-	 */
-	@Override
-	public String toString() {
-		return String.format("Order: %s %d of %s to %s", transactionMode, amount, currencyCode, site);
-	}
 
-	/*
-	 * @see Object - hashCode()  
+	/**
+	 * HashCode algorithm computed on all Order class attributes except the logger
+	 * @see Object#hashCode()
 	 */
 	@Override
 	public int hashCode() {
 		logger.finest("Order hashCode used");
+		
 		final int prime = 31;
-		int result = 1;
+		int result = 13;
+	
+		result = prime * result + site.hashCode();
+		result = prime * result + currencyCode.hashCode();
 		result = prime * result + amount;
-		result = prime * result + ((currencyCode == null) ? 0 : currencyCode.hashCode());
-		result = prime * result + ((site == null) ? 0 : site.hashCode());
-		result = prime * result + ((transactionMode == null) ? 0 : transactionMode.hashCode());
+		result = prime * result + transactionMode.hashCode();
+		
 		return result;
+		
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + amount;
+//		result = prime * result + ((currencyCode == null) ? 0 : currencyCode.hashCode());
+//		result = prime * result + ((site == null) ? 0 : site.hashCode());
+//		result = prime * result + ((transactionMode == null) ? 0 : transactionMode.hashCode());
+//		return result;
 	}
 
-	/*
-	 * @see Object - equals(object)
+	/**
+	 * Equals algorithm includes all Order class attributes except the logger
+	 * @see Object#equals(Object)
 	 */
 	@Override
 	public boolean equals(Object obj) {
 		logger.finest("Order equals used");
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Order other = (Order) obj;
-		if (amount != other.amount)
-			return false;
-		if (currencyCode == null) {
-			if (other.currencyCode != null)
-				return false;
-		} else if (!currencyCode.equals(other.currencyCode))
-			return false;
-		if (site == null) {
-			if (other.site != null)
-				return false;
-		} else if (!site.equals(other.site))
-			return false;
-		if (transactionMode != other.transactionMode)
-			return false;
-		return true;
+		
+		if(obj instanceof Order) {
+			Order that = (Order) obj;
+			if(this.site.equals(that.site)) {
+				if(this.currencyCode.equals(that.currencyCode)) {
+					if(this.amount == that.amount) {
+						return this.transactionMode.equals(that.transactionMode);
+					}
+				}
+			}	
+		}
+		return false;
+		
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Order other = (Order) obj;
+//		if (amount != other.amount)
+//			return false;
+//		if (currencyCode == null) {
+//			if (other.currencyCode != null)
+//				return false;
+//		} else if (!currencyCode.equals(other.currencyCode))
+//			return false;
+//		if (site == null) {
+//			if (other.site != null)
+//				return false;
+//		} else if (!site.equals(other.site))
+//			return false;
+//		if (transactionMode != other.transactionMode)
+//			return false;
+//		return true;
 	}
 
-	/*
-	 * 
+	/**
+	 * Compares Order object by attribute site(alphabetic), currencyCode(alphabetic), 
+	 * transactionMode(alphabetic) and at last amount(highest number first).
+	 * @see Comparable#compareTo(Object)
 	 */
 	@Override
 	public int compareTo(Order that) {
 		logger.finest("Order compareTo used");
-		if(this.amount == that.amount) {
-			if(this.currencyCode.equals(that.currencyCode)) {
-				if(this.transactionMode == that.transactionMode) {
-					return (this.site.compareTo(that.site));
+		
+		int compareVal = this.site.compareTo(that.site);
+		if(compareVal == 0) {	// if same site then compare currencyCode
+			compareVal = this.currencyCode.compareTo(that.currencyCode);
+			
+			if(compareVal == 0) { // if same currencyCode compare transactionMode
+				compareVal = this.transactionMode.toString().compareTo(that.transactionMode.toString());
+				
+				if(compareVal == 0) { // if same transactionMode compare amount
+					return -(this.amount - that.amount);
 				}
-				else {
-					return (this.transactionMode.compareTo(that.transactionMode));
-				}
-			}
-			else {
-				return (this.currencyCode.compareTo(that.currencyCode));
-			}
+			}	
 		}
-		return (this.amount - that.amount);
+		
+		return compareVal;
 	}
 
+	/**
+	 * Converting object data to human readable format
+	 * @return a String using format 
+	 * {"Order [site=<site>, currencyCode=<currencyCode>, amount=<amount>, transactionMode=<transactionMode>]"}
+	 */
+	@Override
+	public String toString() {
+		return String.format("Order [site=%s, currencyCode=%s, amount=%d, transactionMode=%s]"
+				, site, currencyCode, amount, transactionMode);
+	}
 }
