1d36159e7efeaace770b1f8da8004b1852cec9c6
danielskatt <daniel.skatt@gmail.com>
2021-05-02 16:48:04 +0200

    created HQ object

--- HQ/src/main/java/moneyservice/hq/app/HQApp.java
+++ HQ/src/main/java/moneyservice/hq/app/HQApp.java
@@ -22,10 +22,11 @@ import affix.java.project.moneyservice.MoneyServiceIO;
 import affix.java.project.moneyservice.Transaction;
 import affix.java.project.moneyservice.TransactionMode;
 import moneyservice.hq.app.*;
+import moneyservice.model.HQ;
 import moneyservice.model.MoneyServiceSites;
 
 /**
- * This class works as HeadQuarter for one or more Site(s). 
+ * This class works as HeadQuarter for Site(s). 
  * It collects all the statistics from the Site(s) and present it 
  * to the user depending on input
  */
@@ -42,99 +43,99 @@ public class HQApp {
 
 	public static void main(String[] args) {
 
-		// get directory path for HQ project
-		String HQdirPath = System.getProperty("user.dir");
-
 		// store the transaction in a map holding site name and date as key and a list of Transactions a value
-		Map<String, List<Transaction>> siteTransactions = new TreeMap<String, List<Transaction>>();
-		Map<String, List<String>> siteFilenames = new TreeMap<String, List<String>>();
+		Map<String, List<Transaction>> siteTransactions = getTransactions();
+		boolean exit = false;
 
-		// iterate through all Sites
-		for(MoneyServiceSites aSite: MoneyServiceSites.values() ) {
-
-			// get transaction directory path for each site
-			String siteDirPath = HQdirPath + File.separator + "Transactions" + File.separator + aSite;
-
-			try (Stream<Path> walk = Files.walk(Paths.get(siteDirPath))) {
-
-				// Stream for getting the file names
-				List<String> filenameList = walk
-						.map(f -> f.toFile())				// Convert Path to File
-						.map(f -> f.getName())				// Get File name from full path
-						.filter(f -> f.endsWith(".ser"))	// Filter to the files that ends with ".ser"
-						.sorted()							// Sort the files in name ascending order
-						.collect(Collectors.toList());		// Collect them into a List of String
-
-				siteFilenames.putIfAbsent(aSite.getName(), filenameList);
-
-				// Add the files into a Map holding Site name and Date as key, example "SOUTH_2021-04-01" and the Transactions from the file name
-				for(String filename : filenameList) {
-					// add correct path for file name
-					filename = "../HQ/Transactions/" + aSite.getName().toUpperCase() + "/" + filename;
-					// get all the transactions from the file
-					List<Transaction> transactions = MoneyServiceIO.readReportAsSer(filename);
-					// shorten the file name to site name and date for storing as key in map
-					String key = aSite.getName();
-					if(siteTransactions.containsKey(key)) {
-						List<Transaction> newList = siteTransactions.get(key);
-						newList.addAll(transactions);
-						siteTransactions.replace(key, newList);						
-					}
-					else {
-						siteTransactions.putIfAbsent(key, transactions);
-					}
-				}
-
-			} catch (IOException e) {
-				// TODO: Add this to logging?
-			}
-		}
+		HQ theHQ = new HQ("HQ", siteTransactions);
 
 		// user input for choosing which site to filter
 		int siteChoice = presentSiteMenu();
 		for(MoneyServiceSites site : MoneyServiceSites.values()) {
 			if(siteChoice == site.getNumVal()) {
-				if(siteTransactions.containsKey(site.getName())) {
-					List<Transaction> transactions = siteTransactions.get(site.getName());		
-					Optional<LocalDate> endDate = Optional.empty();
-					System.out.println("Entered choice for " + site.getName());
+				if(theHQ.getSiteTransactions().containsKey(site.getName()) || site.getName().equalsIgnoreCase("ALL")) {
 					int period = presentPeriodMenu();
-					Optional<LocalDate> startDate = enterStartDateForPeriod();
-					switch(period){
-					case 1:
-						if(startDate.isPresent()) {
-							endDate = startDate;							
+					while(!exit) {
+						Optional<LocalDate> startDate = enterStartDateForPeriod();
+						Optional<LocalDate> endDate = setEndDate(period, startDate);
+						List<String> availableCodes = theHQ.getCurrencyCodes(site.getName(), startDate.get(), endDate.get());
+						Optional<String> currencyCode = presentCurrencyMenu(availableCodes);
+						if(!currencyCode.isEmpty()) {
+							if(currencyCode.get().equals("T*")) {
+								theHQ.printTransactions(site.getName(), startDate.get(), endDate.get());
+								exit = true;
+							}
+							else {
+								theHQ.printStatistics(site.getName(), currencyCode.get(), startDate.get(), endDate.get());	
+								exit = true;
+							}
 						}
-						break;
-					case 2:
-						if(startDate.isPresent()) {
-							DayOfWeek day = startDate.get().getDayOfWeek();
-							startDate = Optional.of(LocalDate.of(startDate.get().getYear(), startDate.get().getMonthValue(), startDate.get().getDayOfMonth()-(day.getValue()-1)));
-							endDate = Optional.of(LocalDate.of(startDate.get().getYear(), startDate.get().getMonthValue(), startDate.get().getDayOfMonth()+4));							
+						else {
+							exit = false;
 						}
-						break;
-					case 3:
-						if(startDate.isPresent()) {
-							endDate = Optional.of(LocalDate.of(startDate.get().getYear(), startDate.get().getMonthValue(), startDate.get().lengthOfMonth()));							
-						}
-						break;
-					case 0:
-						break;
-					default:
-						break;
-					}
-					List<String> availableCodes = getCurrencyCodes(transactions, startDate.get(), endDate.get());
-					String currencyCode = presentCurrencyMenu(availableCodes);
-					if(currencyCode.equals("T*")) {
-						printTransactions(transactions, startDate.get(), endDate.get());
-					}
-					else {
-						filterStatistics(transactions, currencyCode, startDate.get(), endDate.get());					
 					}
 				}
-
 			}
 		}
+	}
+	
+	/**
+	 * 
+	 * @param filenames
+	 * @param aSite
+	 * @return
+	 */
+	private static Map<String, List<Transaction>> getTransactions(){
+		Map<String, List<Transaction>> siteTransactions = new TreeMap<String, List<Transaction>>();
+		
+		for(MoneyServiceSites aSite : MoneyServiceSites.values()) {
+			List<String> filenames = getFilenames(aSite);
+			// Add the files into a Map holding Site name and Date as key, example "SOUTH_2021-04-01" and the Transactions from the file name
+			for(String filename : filenames) {
+				// add correct path for file name
+				filename = "../HQ/Transactions/" + aSite.getName().toUpperCase() + "/" + filename;
+				// get all the transactions from the file
+				List<Transaction> transactions = MoneyServiceIO.readReportAsSer(filename);
+				// shorten the file name to site name and date for storing as key in map
+				String key = aSite.getName();
+				if(siteTransactions.containsKey(key)) {
+					List<Transaction> newList = siteTransactions.get(key);
+					newList.addAll(transactions);
+					siteTransactions.replace(key, newList);						
+				}
+				else {
+					siteTransactions.putIfAbsent(key, transactions);
+				}
+			}
+			
+		}
+		return siteTransactions;
+	}
+	
+	/**
+	 * 
+	 * @param path
+	 * @return
+	 */
+	private static List<String> getFilenames(MoneyServiceSites aSite){
+		List<String> filenameList = new ArrayList<String>();
+		// get directory path for HQ project
+		String HQdirPath = System.getProperty("user.dir");
+		// get transaction directory path for each site
+		String path = HQdirPath + File.separator + "Transactions" + File.separator + aSite;
+		try (Stream<Path> walk = Files.walk(Paths.get(path))) {
+			// Stream for getting the file names
+			filenameList = walk
+					.map(f -> f.toFile())				// Convert Path to File
+					.map(f -> f.getName())				// Get File name from full path
+					.filter(f -> f.endsWith(".ser"))	// Filter to the files that ends with ".ser"
+					.sorted()							// Sort the files in name ascending order
+					.collect(Collectors.toList());		// Collect them into a List of String
+
+		} catch (IOException e) {
+			// TODO: Add this to logging?
+		}
+		return filenameList;
 	}
 
 	/**
@@ -153,7 +154,6 @@ public class HQApp {
 			}
 			return num;
 		}
-
 		String input = keyboard.next();
 		System.out.println(input + " is not a valid number!");
 		return errorNo;
@@ -173,12 +173,12 @@ public class HQApp {
 		// present site menu
 		System.out.println("--------------------------------------------------");
 		System.out.format("Choose a Site%n%n");
-		System.out.format("1 - North%n");
-		System.out.format("2 - East%n");
-		System.out.format("3 - Center%n");
-		System.out.format("4 - South%n");
-		System.out.format("5 - All%n");
-		System.out.format("0 - Exit%n%n");	// EXIT
+		for(MoneyServiceSites site : MoneyServiceSites.values()) {
+			if(!site.getName().equalsIgnoreCase("None")) {
+				System.out.format("%d - %s%n", site.getNumVal(), site.getName());				
+			}
+		}
+		System.out.format("0 - Exit%n%n");
 
 		System.out.format("Enter your choice: ");
 
@@ -265,121 +265,80 @@ public class HQApp {
 	}
 	
 	/**
-	 * This method gets all available currency codes from a map
-	 * @param transactions - a Map with all Transaction(s)
-	 * @return - A List with String of Currency codes
+	 * 
+	 * @param period
+	 * @param startDate
+	 * @return
 	 */
-	private static List<String> getCurrencyCodes(List<Transaction> transactions, LocalDate startDate, LocalDate endDate){		
-		// get all the available currency codes with no doubles
-		List<String> availableCodes = transactions
-				.stream()							// start a stream
-				.filter(t -> t.getTimeStamp().toLocalDate().isEqual(startDate) ||
-						(t.getTimeStamp().toLocalDate().isAfter(startDate) &&
-						t.getTimeStamp().toLocalDate().isBefore(endDate)) ||
-						t.getTimeStamp().toLocalDate().isEqual(endDate))
-				.map(t -> t.getCurrencyCode())		// convert the stream to only handle currency codes
-				.distinct()							// sort the currency code in alphabetic order
-				.collect(Collectors.toList());		// collect all available element to a List
-		
-		return availableCodes;
+	private static Optional<LocalDate> setEndDate(int period, Optional<LocalDate> startDate){
+		Optional<LocalDate> endDate = Optional.empty();
+		switch(period){
+		case 1:
+			if(startDate.isPresent()) {
+				endDate = startDate;							
+			}
+			break;
+		case 2:
+			if(startDate.isPresent()) {
+				DayOfWeek day = startDate.get().getDayOfWeek();
+				if(startDate.get().getDayOfMonth() - day.getValue() < 1) {
+					int toFriday = 5 - day.getValue();
+					endDate = Optional.of(LocalDate.of(startDate.get().getYear(), startDate.get().getMonthValue(), startDate.get().getDayOfMonth()+toFriday));
+				}
+				else {
+					startDate = Optional.of(LocalDate.of(startDate.get().getYear(), startDate.get().getMonthValue(), startDate.get().getDayOfMonth()-(day.getValue()-1)));
+					if(startDate.get().getDayOfMonth() + 4 <= startDate.get().lengthOfMonth()) {
+						endDate = Optional.of(LocalDate.of(startDate.get().getYear(), startDate.get().getMonthValue(), startDate.get().getDayOfMonth()+4));																		
+					}
+					else {
+						endDate = Optional.of(LocalDate.of(startDate.get().getYear(), startDate.get().getMonthValue(), startDate.get().lengthOfMonth()));
+					}
+				}
+			}
+			break;
+		case 3:
+			if(startDate.isPresent()) {
+				endDate = Optional.of(LocalDate.of(startDate.get().getYear(), startDate.get().getMonthValue(), startDate.get().lengthOfMonth()));							
+			}
+			break;
+		case 0:
+			break;
+		default:
+			break;
+		}
+		return endDate;
 	}
 	
 	/**
 	 * This method presents the available currencies and gets input for currency from user
 	 * @return a String from user input holding the currency or ALL for all currencies
 	 */
-	private static String presentCurrencyMenu(List<String> currencyCodes) {
-		boolean correctCode = false;
-		String input = "";
-		while(!correctCode) {
+	private static Optional<String> presentCurrencyMenu(List<String> currencyCodes) {
+		boolean exit = false;
+		String input = null;
+		while(!exit) {
 			System.out.print("Available currency codes: ");
-			for(String currency : currencyCodes) {
-				System.out.print(currency + " ");
+			if(currencyCodes.isEmpty()) {
+				System.out.println("No available currencies...");
+				exit = true;
 			}
-			System.out.print("ALL ");
-			input = keyboard.next();
-			for(String match : currencyCodes) {
-				if(input.equals(match) || input.equals("ALL") || input.equals("T*")) {
-					correctCode = true;
+			else {
+				for(String currency : currencyCodes) {
+					System.out.print(currency + " ");
 				}
+				System.out.print("ALL ");
+				input = keyboard.next();
+				for(String match : currencyCodes) {
+					if(input.equals(match) || input.equals("ALL") || input.equals("T*")) {
+						exit = true;
+					}
+					else {
+						System.out.println("Not a valid currency or code");
+					}
+				}				
 			}
 		}
-		return input;
+		return input != null ? Optional.of(input) : Optional.empty();
 	}
-	
-	/**
-	 * This method filter the available statistics and present it to the user
-	 * @param allTransactions - A List with all Transactions
-	 * @param currencyCode - A String holding the desired currency code or ALL
-	 * @param startDate - a LocalDate holding information about which period the user want to filter the statistics
-	 * @param endDate - a LocalDate holding information about the start date of the period
-	 */
-	private static void filterStatistics(List<Transaction>transactions, String currencyCode, LocalDate startDate, LocalDate endDate) {
-		IntSummaryStatistics sell;
-		IntSummaryStatistics buy;
-		if(currencyCode.equals("ALL")) {
-			sell = transactions
-					.stream()
-					.filter(cc -> cc.getMode().equals(TransactionMode.SELL))
-					.filter(t -> t.getTimeStamp().toLocalDate().isEqual(startDate) ||
-							(t.getTimeStamp().toLocalDate().isAfter(startDate) &&
-							t.getTimeStamp().toLocalDate().isBefore(endDate)) ||
-							t.getTimeStamp().toLocalDate().isEqual(endDate))
-					.collect(Collectors.summarizingInt(Transaction::getAmount));
-			
-			buy = transactions
-					.stream()
-					.filter(cc -> cc.getMode().equals(TransactionMode.BUY))
-					.filter(t -> t.getTimeStamp().toLocalDate().isEqual(startDate) ||
-							(t.getTimeStamp().toLocalDate().isAfter(startDate) &&
-							t.getTimeStamp().toLocalDate().isBefore(endDate)) ||
-							t.getTimeStamp().toLocalDate().isEqual(endDate))
-					.collect(Collectors.summarizingInt(Transaction::getAmount));
-		}
-		else {
-			sell = transactions
-					.stream()
-					.filter(cc -> cc.getCurrencyCode().equals(currencyCode))
-					.filter(cc -> cc.getMode().equals(TransactionMode.SELL))
-					.filter(t -> t.getTimeStamp().toLocalDate().isEqual(startDate) ||
-							(t.getTimeStamp().toLocalDate().isAfter(startDate) &&
-							t.getTimeStamp().toLocalDate().isBefore(endDate)) ||
-							t.getTimeStamp().toLocalDate().isEqual(endDate))
-					.collect(Collectors.summarizingInt(Transaction::getAmount));
-			
-			buy = transactions
-					.stream()
-					.filter(cc -> cc.getCurrencyCode().equals(currencyCode))
-					.filter(cc -> cc.getMode().equals(TransactionMode.BUY))
-					.filter(t -> t.getTimeStamp().toLocalDate().isEqual(startDate) ||
-							(t.getTimeStamp().toLocalDate().isAfter(startDate) &&
-							t.getTimeStamp().toLocalDate().isBefore(endDate)) ||
-							t.getTimeStamp().toLocalDate().isEqual(endDate))
-					.collect(Collectors.summarizingInt(Transaction::getAmount));			
-		}
-		
-		System.out.println("Total   " + TransactionMode.SELL.name() + "  " + sell.getSum() + " SEK");
-		System.out.println("Total   " + TransactionMode.BUY.name() + "  " + buy.getSum() + " SEK");
-		System.out.println("Profit " + (sell.getSum() - buy.getSum()) + " SEK");
-	}
-	
-	/**
-	 * Print out all the transaction for the date provided
-	 * @param transactions - a Map with all the transactions
-	 * @param date - a start date for filtering the transactions
-	 */
-	private static void printTransactions(List<Transaction> transactions, LocalDate startDate, LocalDate endDate) {
-		List<Transaction> allTransactions = transactions
-				.stream()													// start a stream of the values
-				.filter(t -> t.getTimeStamp().toLocalDate().isEqual(startDate) ||
-						(t.getTimeStamp().toLocalDate().isAfter(startDate) &&
-						t.getTimeStamp().toLocalDate().isBefore(endDate)) ||
-						t.getTimeStamp().toLocalDate().isEqual(endDate))
-				.distinct()													// make sure only has one of each element
-				.collect(Collectors.toList());								// collect all the elements to a List
-		
-		allTransactions.forEach(System.out::println);
-	}
-
 }
 
